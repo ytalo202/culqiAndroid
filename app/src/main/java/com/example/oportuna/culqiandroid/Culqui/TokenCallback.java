183894b8a9dadfbd77b986f2b9caafd6fc59fe9c
yoshinon <tuestaytalo@gmail.com>
2018-12-03 16:39:26 -0500

    Initial commit

---
+++ app/src/main/java/com/example/oportuna/culqiandroid/Culqui/TokenCallback.java
@@ -0,0 +1,15 @@
+package com.example.oportuna.culqiandroid.Culqui;
+
+import org.json.JSONObject;
+
+/**
+ * Created by culqi on 2/7/17.
+ */
+
+public interface TokenCallback {
+
+    public void onSuccess(JSONObject token);
+
+    public void onError(Exception error);
+
+}
